f4416263a6e1a140f2a4b6dfe811bd7c08c4fd0e
u79318 <u79318@login-2.aidevcloud>
2021-08-28 05:17:17 -0700

    Parallelized other Functions and tested

--- utils/test.cpp
+++ utils/test.cpp
@@ -5,34 +5,33 @@
 
 using namespace std;
 
-constexpr int M=40;
-constexpr int N=40;
+constexpr int M=10;
+constexpr int N=10;
 constexpr int P=10;
 
 int main()
 {
-    vector<vector<float>> A;
-    vector<vector<float>> B;
-    vector<vector<float>> C;
+    vector<float> C;
+    vector<float> A;
+    vector<float> B;
     
     for(int i=0;i<M;++i)
     {
-        A.push_back(vector<float>());
-        for(int j=0;j<N;++j) 
-            A.back().push_back(5.0f);
+        //A.push_back(vector<float>());
+        //for(int j=0;j<N;++j) 
+            A.push_back(5.0f);
     }
-    for(int i=0;i<M;++i)
+    for(int i=0;i<N;++i)
     {
-        B.push_back(vector<float>());
-        for(int j=0;j<N;++j)
-            B.back().push_back(10.0f);
+        //B.push_back(vector<float>());
+        //for(int j=0;j<N;++j)
+            B.push_back(i+1.0f);
     }
-    C = util::add(A, B);
-    /*C.assign(C.begin(),C.end()-1);*/
+    C = util::hadamardProduct(A,B);
     for(int i=0;i<C.size();++i)
     {
-        for(int j=0;j<C[0].size();++j)
-            cout << C[i][j] << " ";
+        //for(int j=0;j<C[0].size();++j)
+            cout << C[i] << " ";
         cout << std::endl;
     }
 
--- utils/util.h
+++ utils/util.h
@@ -37,7 +37,6 @@ vector<vector<T>> util::add(vector<vector<T>> &a,vector<vector<T>> &b)
     try
     {
         queue q(gpu_selector{}, dpc_common::exception_handler);
-        cout << "GPU Device: " << q.get_device().get_info<info::device::name>() << std::endl;
         for(int i=0;i<a.size();++i)
         {
             buffer C{c[i]};
@@ -71,10 +70,14 @@ vector<T> util::innerProduct(vector<vector<T>> &a, vector<T> &b)
     
     assert(a[0].size()==b.size());
 
-    for(int i=0;i<a[0].size();++i)
+    for(int i=0;i<a.size();++i)
         c.push_back(0.0f);
+
     try
     {
+        queue q(gpu_selector{}, dpc_common::exception_handler);
+        cout << "GPU Device: " << q.get_device().get_info<info::device::name>() << std::endl;
+
         buffer B{b};
         buffer C{c};
         for(int i=0;i<a.size();++i)
@@ -85,12 +88,16 @@ vector<T> util::innerProduct(vector<vector<T>> &a, vector<T> &b)
             q.submit([&](auto &h){
                     accessor A1(A, h, read_only);
                     accessor B1(B, h, read_only);
-                    accessor C1(C, h, write_only);
+                    accessor C1(C, h, read_only);
 
                     h.parallel_for(range(a[0].size()), [=](auto index){
-                            C1[i] = A1[index]*B1[index];
+                            C1[i] += A1[index]*B1[index];
                             });
                     });
+            host_accessor C1(C);
+            for(int j=0;j<a.size();++j)
+                cout <<C1[j] << " ";
+            cout << std::endl;
         }
     }
     catch (sycl::exception const &e) {
@@ -110,8 +117,35 @@ vector<vector<T>> util::innerProduct(vector<T> &a, vector<T> &b)
     {
         c.push_back(vector<T>());
         for(int j=0;j<b.size();++j)
-            c.back().push_back(a[i]*b[j]);
+            c.back().push_back(0.0f);
     }
+
+    try
+    {
+        queue q(gpu_selector{}, dpc_common::exception_handler);
+
+        buffer A{a};
+        buffer B{b};
+        for(int i=0;i<a.size();++i)
+        {
+            buffer C{c[i]};
+
+            q.submit([&](auto &h){
+                    accessor A1(A, h, read_only);
+                    accessor B1(B, h, read_only);
+                    accessor C1(C, h, write_only);
+
+                    h.parallel_for(range(b.size()), [=](auto index){
+                            C1[index] = A1[i] * B1[index];
+                            });
+                    });
+        }
+    }
+    catch (sycl::exception const &e) {
+        cout << "An exception is caught while adding matrices.\n";
+        terminate();
+    }
+
     return c;
 }
 
@@ -138,11 +172,35 @@ vector<T> util::hadamardProduct(vector<T> &a, vector<T> &b)
     vector<T> c;
 
     assert(a.size()==b.size());
-    for(int i=0;i<a.size();++i)
+    for(int i=0;i<b.size();++i)
     {
-        c.push_back(a[i]*b[i]);
+            c.push_back(1.0f);
     }
 
+    try
+    {
+        queue q(gpu_selector{}, dpc_common::exception_handler);
+        cout << "GPU Device: " << q.get_device().get_info<info::device::name>() << std::endl;
+
+            buffer C{c};
+            buffer A{a};
+            buffer B{b};
+
+
+            q.submit([&](auto &h){
+                    accessor A1(A, h, read_only);
+                    accessor B1(B, h, read_only);
+                    accessor C1(C, h, write_only);
+
+                    h.parallel_for(range(b.size()),[=](auto index){
+                            C1[index] = A1[index]*B1[index];
+                            });
+                    });
+        }
+    catch (sycl::exception const &e) {
+        cout << "An exception is caught while adding matrices.\n";
+        terminate();
+    }
     return c;
 }
 
@@ -155,7 +213,32 @@ vector<vector<T>> util::product(U n, vector<vector<T>> &a)
     {
         c.push_back(vector<T>());
         for(int j=0;j<a[0].size();++j)
-            c.back().push_back(n*a[i][j]);
+            c.back().push_back(0.0f);
+    }
+
+    try
+    {
+        queue q(gpu_selector{}, dpc_common::exception_handler);
+        cout << "GPU Device: " << q.get_device().get_info<info::device::name>() << std::endl;
+
+        for(int i=0;i<a.size();++i)
+        {
+            buffer A{a[i]};
+            buffer C{c[i]};
+
+            q.submit([&](auto &h){
+                    accessor A1(A, h, read_only);
+                    accessor C1(C, h, write_only);
+
+                    h.parallel_for(range(a[0].size()), [=](auto index){
+                            C1[index] = n*A1[index];
+                            });
+                    });
+        }
+    }
+    catch (sycl::exception const &e) {
+        cout << "An exception is caught while adding matrices.\n";
+        terminate();
     }
 
     return c;
